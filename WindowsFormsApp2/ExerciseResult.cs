41731d3a197de3faadcd922fddc7cda836459e3a
nikolaMilakovic <nikola.milakovic1999@gmail.com>
2020-11-09 23:17:27 +0100

    Napravljeni geteri i seteri, konstruktor i toString metoda
    Signed-off-by: radulovicDarko <darkodarko99@gmail.com>

---
+++ WindowsFormsApp2/ExerciseResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace WindowsFormsApp2
+{
+    class ExerciseResult
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string StudentIndex { get; set; }
+        public int Points { get; set; }
+
+        public ExerciseResult (int Id, string StudentName, string StudentIndex, int Points)
+        {
+            this.Id = Id;
+            this.StudentName = StudentName;
+            this.StudentIndex = StudentIndex;
+            this.Points = Points;
+         }
+
+        public override string ToString()
+        {
+            return Id+". "+StudentName+" ("+StudentIndex+") Broj poena --> "+Points;
+        }
+    }
+}
