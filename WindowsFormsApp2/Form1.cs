adeca4eef2416bec07717ac846b3f8ad2a918a66
nikolaMilakovic <nikola.milakovic1999@gmail.com>
2020-11-09 23:43:16 +0100

    Povezana je baza
    Signed-off-by: radulovicDarko <darkodarko99@gmail.com>

--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,31 @@ namespace WindowsFormsApp2
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<ExerciseResult> lista = new List<ExerciseResult>();
+            string connString = "Data Source=(localdb)\\ProjectsV13;Initial Catalog=FacultyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            using (
+            SqlConnection dataConnection = new SqlConnection(connString))
+            {
+                dataConnection.Open();
+                SqlCommand command = new SqlCommand();
+                command.Connection = dataConnection;
+                command.CommandText = "SELECT  *  FROM  ExerciseResults";
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    ExerciseResult pom = new ExerciseResult(dataReader.GetInt32(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetInt32(3));
+
+
+                    lista.Add(pom);
+                }
+
+                foreach (ExerciseResult er in lista)
+                    listBoxExerciseResults.Items.Add(er.ToString());
+            }
+        }
     }
 }
